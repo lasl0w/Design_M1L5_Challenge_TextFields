1494fac3b0f31abab255a8cce245187b2ac9586b
Tom Montgomery <montgomery3681@gmail.com>
2023-09-14 19:00:29 -0600

    Initial Commit

---
+++ Design_M1L5_Challenge_TextFields/Design_M1L5_Challenge_TextFieldsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Design_M1L5_Challenge_TextFieldsApp.swift
+//  Design_M1L5_Challenge_TextFields
+//
+//  Created by tom montgomery on 9/14/23.
+//
+
+import SwiftUI
+
+@main
+struct Design_M1L5_Challenge_TextFieldsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
